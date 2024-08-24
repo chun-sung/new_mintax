3d872cc3ab43972e090cb9a466330dd2f426cf95
unknown <you@example.com>
2024-08-24 16:57:21 +0900

    20240824_1657

---
+++ src/components/Login.js
@@ -0,0 +1,33 @@
+'use client'
+
+export default function Login() {
+
+  return (
+		<div className="absolute border-stone-400 border-[1px] bg-gray-100  w-full lg:w-[380px] lg:mt-10 top-[140px] lg:top-[200px] p-5 h-58 shadow-2xl z-10 rounded left-[50%] translate-x-[-50%]">
+			<div className="text-center mb-2">                   
+					<span className="text-md text-black">Welcome to MinTax</span>
+			</div>
+			<div className="lg:ml-[-28px] lg:w-96 mb-3  text-center">   
+					<form>
+							<div className="relative sm:mb-0 flex-grow w-full mb-2">
+									<label htmlFor="full-name" className="leading-7 text-[12px] text-gray-600 mr-2">ID </label>
+									<input type="text" id="full-name" name="user_id" className="w-56 h-9 bg-gray-200 bg-opacity-50 rounded border border-gray-400 focus:border-indigo-100 focus:ring-2 focus:ring-indigo-400 focus:bg-transparent text-md outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
+									/>
+							</div>
+							<div className="relative sm:mb-0 flex-grow w-full mt-2 ml-[-4px]">
+									<label htmlFor="name" className="leading-7 text-[12px] text-gray-600 mr-2">PW </label>
+									<input type="password" id="name" name="password" className="w-56 h-9 bg-gray-200 bg-opacity-50 ml-[0.5px] rounded border border-gray-400 focus:border-indigo-100 focus:ring-2 focus:ring-indigo-400 focus:bg-transparent text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onKeyUp={()=>{ window.event.keyCode === 13 ? loginEnter() : null }}/>
+							</div>
+					</form>
+			</div>
+			<div className="mt-2 ml-6 text-center text-sm">
+					<button className="p-1.5 px-4 shadow rounded mr-2 bg-blue-500 hover:bg-blue-400 text-white" onClick={()=> { 
+						// return loginEnter()
+					}}>확인</button>
+
+					<button className="p-1.5 px-4 shadow rounded bg-red-500 hover:bg-red-300 text-white" onClick={()=> {
+							// dispatch(SET_LOGIN_WINDOW(false))
+					}}>취소</button>
+			</div>   
+  	</div>);
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
 
-  return (<div className="h-[50px] bg-slate-900 text-white lg:block hidden">        
+  return (<div className="h-[50px] bg-slate-900 text-white lg:block hidden">
    <div className="w-[1000px] h-full text-center m-auto leading-[50px]">
       <Link className="mr-10 text-xl" href="#">🌞</Link>
       <Link className="mr-10 text-md hover:text-blue-300" href={'/smart'}>SMART서비스</Link>
@@ -10,9 +10,11 @@ export default function Navbar() {
       <Link className="mr-10 text-md hover:text-blue-300" href={'/bubin'}>법인 컨설팅</Link>
       <Link className="mr-10 text-md hover:text-blue-300" href={'/consult'}>세무상담</Link>    
       <Link className="mr-10 text-md hover:text-blue-300" href={'/susu'}>수수료안내</Link>
-      <Link className="mr-[90px] text-md hover:text-blue-300" href={'/board'}>게시판</Link>         
-      <Link className="border bg-slate-700  hover:text-yellow-300 w-[80px] h-[30px] text-sm text-center rounded-md p-1 px-3" href="/login">Login</Link>
-        
-    </div>
+      <Link className="mr-[50px] text-md hover:text-blue-300" href={'/board'}>게시판</Link>      
+      <button className="mr-[10px] border-[0.5px] bg-white text-black hover:text-pink-500 w-[70px] h-[30px] text-sm text-center rounded-2xl p-1 px-3"
+        >Login</button>        
+      <button className="border-[0.5px] bg-blue-600 hover:text-gray-300 w-[80px] h-[30px] text-sm text-center rounded-2xl p-1 px-3"
+        >Sign up</button>        
+    </div>        
   </div>);
 }
