5d04fe02bd63e13da0eaf5f8c01dc4c1d41ca492
unknown <you@example.com>
2024-08-27 17:00:22 +0900

    20240728_1700_mobile_menu_btn

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,38 +7,39 @@ export default function Navbar() {
 
   const pathname = usePathname();
 
-  return (<div className="h-[50px] bg-slate-900 text-white lg:block hidden">
-   <div className="w-[1000px] h-full text-center m-auto leading-[50px]">
-      <Link className="mr-5 text-xl" href="#">🌞</Link>
+  return (<nav className="lg:h-[50px] border rounded bg-white lg:bg-slate-900 lg:text-white lg:block">
+   <div className="mt-3 lg:mt-0 lg:w-[1000px] h-full text-center m-auto leading-[50px]">
+      <Link className="mr-0 lg:mr-5 text-xl block hidden lg:inline" href="#">🌞</Link>
       <Link href={'/smart'}
-        className={clsx("mr-5 text-md py-1 px-2 rounded-lg border border-black hover:border-gray-500",{
-          'bg-slate-600': pathname === '/smart'
+        className={clsx("border-b ml-4 lg:ml-0 block text-left lg:inline mr-5 text-md py-1 px-2 lg:rounded-lg lg:border lg:border-black hover:border-gray-500",{
+          'lg:bg-slate-600 bg-red-300': pathname === '/smart'
         })}>SMART서비스
       </Link>
       <Link href={'/himoney'} 
-        className={clsx("mr-5 text-md py-1 px-2 rounded-lg border border-black hover:border-gray-500",{
-          'bg-slate-600': pathname === '/himoney'
+        className={clsx("border-b ml-4 lg:ml-0 text-left block lg:inline mr-5 text-md py-1 px-2 lg:rounded-lg lg:border lg:border-black hover:border-gray-500",{
+          'lg:bg-slate-600 bg-red-300': pathname === '/himoney'
         })}>고소득자 플랜</Link>
       <Link  href={'/bubin'} 
-        className={clsx("mr-5 text-md py-1 px-2 rounded-lg border border-black hover:border-gray-500",{
-          'bg-slate-600': pathname === '/bubin'
+        className={clsx("border-b ml-4 lg:ml-0 text-left block lg:inline mr-5 text-md py-1 px-2 lg:rounded-lg lg:border lg:border-black hover:border-gray-500",{
+          'lg:bg-slate-600 bg-red-300': pathname === '/bubin'
         })}>법인 컨설팅</Link>
       <Link  href={'/consult'} 
-        className={clsx("mr-5 text-md py-1 px-2 rounded-lg border border-black hover:border-gray-500",{
-          'bg-slate-600': pathname === '/consult'
+        className={clsx("border-b ml-4 lg:ml-0 text-left block lg:inline mr-5 text-md py-1 px-2 lg:rounded-lg lg:border lg:border-black hover:border-gray-500",{
+          'lg:bg-slate-600 bg-red-300': pathname === '/consult'
         })}>세무상담</Link>    
       <Link href={'/susu'}
-        className={clsx("mr-5 text-md py-1 px-2 rounded-lg border border-black hover:border-gray-500",{
-          'bg-slate-600': pathname === '/susu'
+        className={clsx("border-b ml-4 lg:ml-0 text-left block lg:inline mr-5 text-md py-1 px-2 lg:rounded-lg lg:border lg:border-black hover:border-gray-500",{
+          'lg:bg-slate-600 bg-red-300': pathname === '/susu'
         })}>수수료안내</Link>
       <Link href={'/board'}
-        className={clsx("mr-[50px] text-md py-1 px-2 rounded-lg border border-black hover:border-gray-500",{
-          'bg-slate-600': pathname === '/board'
+        className={clsx("border-b ml-4 lg:ml-0 text-left block lg:inline mr-5 lg:mr-[50px] text-md py-1 px-2 lg:rounded-lg lg:border lg:border-black hover:border-gray-500",{
+          'lg:bg-slate-600 bg-red-300': pathname === '/board'
         })} >게시판</Link>      
-      <button className="mr-[10px] border-[0.5px] bg-white text-black hover:text-pink-500 w-[70px] h-[30px] text-sm text-center rounded-2xl p-1 px-3"
+      <button className="mt-10 lg:mt-0 mr-[10px] border-[0.5px] bg-gray-300 lg:bg-white text-black hover:text-pink-500 w-[70px] h-[30px] text-sm text-center rounded-2xl p-1 px-3"
         >Login</button>        
-      <button className="border-[0.5px] bg-blue-600 hover:text-gray-300 w-[80px] h-[30px] text-sm text-center rounded-2xl p-1 px-3"
-        >Sign up</button>        
+      <button className="mt-10 lg:mt-0 border-[0.5px] bg-blue-600 hover:text-gray-300 w-[80px] h-[30px] text-sm text-center rounded-2xl p-1 px-3"
+        >Sign up</button>
+      <Link className="mr-0 lg:mr-5 mt-5 text-sm block lg:inline lg:hidden" href="#">DarkMode: 🌞</Link>        
     </div>        
-  </div>);
+  </nav>);
 }
