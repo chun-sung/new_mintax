8f81df6206e6926e0241d72c7c8aee803af02d7c
SP_k <springstar74@gmail.com>
2024-08-30 00:59:32 +0900

    20240830_0052_Member_Panel_Complete2

--- src/components/Mobile_btn.js
+++ src/components/Mobile_btn.js
@@ -1,7 +1,7 @@
 'use client'
 import Image from "next/image";
 import { useDispatch, useSelector } from "react-redux";
-import { SET_LOGIN_WINDOW, SET_MENU_BTN } from  "../redux/reducers/userSlice";
+import { SET_LOGIN_WINDOW, SET_MEMBER_PANEL, SET_MENU_BTN } from  "../redux/reducers/userSlice";
 
 export default function Mobile_btn() {
 
@@ -11,9 +11,10 @@ export default function Mobile_btn() {
     return (
         <Image className="absolute top-[20px] left-[15px] hover:scale-110 hover:border-[2px] border-red-300 lg:hidden block"                
             onClick={() => {
-                dispatch(SET_MENU_BTN(!user.menu));dispatch(SET_LOGIN_WINDOW(false))
-                console.log(user.menu)}
-            }
+                dispatch(SET_MENU_BTN(!user.menu));
+                dispatch(SET_LOGIN_WINDOW(false));
+                dispatch(SET_MEMBER_PANEL(false));
+            }}
                 src="/hamburger.svg"
                 width="40"
                 height="40"
