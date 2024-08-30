6c3a0f8297f2c5fab2caf133d6ac3aad0b381cf6
SP_k <springstar74@gmail.com>
2024-08-30 16:39:37 +0900

    20240830_1639

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,20 +14,20 @@ export default function Header() {
     const router = useRouter();
 
 // 쿠키에 최초 라이트 모드 값을 셋팅 한다.
-  useEffect(()=> {
-    setMode(('; '+document.cookie).split(`; mode=`).pop().split(';')[0])     // mode 가 없으면 '' 리턴 됨                
+//   useEffect(()=> {
+//     setMode(('; '+document.cookie).split(`; mode=`).pop().split(';')[0])     // mode 가 없으면 '' 리턴 됨                
 
-    // console.log(('; '+document.cookie).split(`; mode=`).pop().split(';')[0])
+//     // console.log(('; '+document.cookie).split(`; mode=`).pop().split(';')[0])
 
-    // 초기값 비교는 state 함수가 비동기라 비교하여 적용할 수 없다. 비교는 쿠키에 직접 접근하는 코드로 비교해야 한다.
-    if(('; '+document.cookie).split(`; mode=`).pop().split(';')[0] == '' || ('; '+document.cookie).split(`; mode=`).pop().split(';')[0] == 'lightMode') {
-        document.cookie = 'mode=lightMode; max-age=' + (3600 * 24 * 400)
-        setMode('lightMode')
-    } else {
-        // document.cookie = 'mode=darkMode; max-age=' + (3600 * 24 * 400)
-        setMode('darkMode')
-    }
-  },[])
+//     // 초기값 비교는 state 함수가 비동기라 비교하여 적용할 수 없다. 비교는 쿠키에 직접 접근하는 코드로 비교해야 한다.
+//     if(('; '+document.cookie).split(`; mode=`).pop().split(';')[0] == '' || ('; '+document.cookie).split(`; mode=`).pop().split(';')[0] == 'lightMode') {
+//         document.cookie = 'mode=lightMode; max-age=' + (3600 * 24 * 400)
+//         setMode('lightMode')
+//     } else {
+//         // document.cookie = 'mode=darkMode; max-age=' + (3600 * 24 * 400)
+//         setMode('darkMode')
+//     }
+//   },[])
 
     return (<>
     <Modal />
