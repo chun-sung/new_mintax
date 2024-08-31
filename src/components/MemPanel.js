adc441cb071894823619e8a7fdf9983132a2edc9
SP_k <springstar74@gmail.com>
2024-08-31 23:47:24 +0900

    20240831_2350

--- src/components/MemPanel.js
+++ src/components/MemPanel.js
@@ -17,33 +17,33 @@ export default function MemPanel({mode}) {
     { user?.member_panel == true ? 
         <div className="relative m-auto w-[320px] lg:w-[420px]">
         <div className={ mode == 'lightMode' ? "absolute border-stone-400 border-[1px] bg-white w-[320px] lg:w-[380px] pt-4 pb-6 lg:mt-10 lg:left-[20px] top-[70px] lg:top-[90px] lg:p-5 h-180 shadow-2xl z-50 rounded"
-            : "absolute border-stone-400 border-[1px] bg-gray-500 w-[320px] lg:w-[380px] pt-2 pb-2 lg:mt-10 lg:left-[20px] top-[70px] lg:top-[90px] lg:p-5 h-180 shadow-2xl z-50 rounded"
+            : "absolute border-stone-400 border-[1px] bg-gray-500 w-[320px] lg:w-[380px] pt-4 pb-6 lg:mt-10 lg:left-[20px] top-[70px] lg:top-[90px] lg:p-5 h-180 shadow-2xl z-50 rounded"
         }>
             <div className="text-center mb-2">
                 {/* <span>전화번호 입력 후 확인을 눌러주세요</span><br /> */}
-                <span className="text-md ml-[20px] lg:ml-[20px] text-black stop-dragging">Welcome to Register</span>
+                <span className="text-md ml-[0px] lg:ml-[10px] text-black stop-dragging">Welcome to Register</span>
             </div>
             <div className="lg:ml-[20px] lg:w-96 mb-3">   
                 <form>
-                    <div className="relative sm:mb-0 flex-grow w-full mb-2 ml-[40px] lg:ml-[30px]">
+                    <div className="relative sm:mb-0 flex-grow w-full mb-2 ml-[30px] lg:ml-[20px]">
                         <label htmlFor="id" className="leading-7 text-[12px] text-gray-600 mr-4">ID</label>
                         <input autoComplete="off" type="text" id="id" name="id" className="w-52 h-9 bg-gray-200 bg-opacity-40 rounded border border-gray-400 focus:border-indigo-100 focus:ring-2 focus:ring-indigo-400 focus:bg-transparent text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
                             onChange={(e) => setUserId(e.target.value)} 
                         />
                     </div>
-                    <div className="relative sm:mb-0 flex-grow w-full mt-2 ml-[31px] lg:ml-[18px]">
+                    <div className="relative sm:mb-0 flex-grow w-full mt-2 ml-[21px] lg:ml-[8px]">
                         <label htmlFor="nickName" className="leading-7 text-[12px] text-gray-600 mr-4">이름</label>
                         <input autoComplete="off" type="text" id="nickName" name="nickName" className="w-52 h-9 bg-gray-200 bg-opacity-40 rounded border border-gray-400 focus:border-indigo-100 focus:ring-2 focus:ring-indigo-400 focus:bg-transparent text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
                             onChange={(e) => setNickName(e.target.value)}
                         />
                     </div>
-                    <div className="relative sm:mb-0 flex-grow w-full mt-2 ml-[18px] lg:ml-[4px]">
+                    <div className="relative sm:mb-0 flex-grow w-full mt-2 ml-[8px] lg:ml-[-4px]">
                         <label htmlFor="pw" className="leading-7 text-[12px] text-gray-600 mr-2">비밀번호</label>
                         <input autoComplete="off" type="password" id="pw" name="password" className="w-52 h-9 bg-gray-200 bg-opacity-40 rounded border border-gray-400 focus:border-indigo-100 focus:ring-2 focus:ring-indigo-400 focus:bg-transparent text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
                             onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
-                    <div className="relative sm:mb-0 flex-grow w-full mt-2 ml-[18px] lg:ml-[4px]">
+                    <div className="relative sm:mb-0 flex-grow w-full mt-2 ml-[8px] lg:ml-[-4px]">
                         <label htmlFor="name2" className="leading-7 text-[12px] text-gray-600 mr-2">비번확인</label>
                         <input autoComplete="off" type="password" id="name2" name="password2" className="w-52 h-9 bg-gray-200 bg-opacity-40 rounded border border-gray-400 focus:border-indigo-100 focus:ring-2 focus:ring-indigo-400 focus:bg-transparent text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
                             onChange={(e) => setPwCheck(e.target.value)}
@@ -51,7 +51,7 @@ export default function MemPanel({mode}) {
                     </div>
                 </form>
             </div>
-            <div className="mt-2 ml-[20px] lg:ml-[35px] text-center text-sm">
+            <div className="mt-2 ml-[10px] lg:ml-[15px] text-center text-sm">
                 <button className="p-1.5 px-4 shadow rounded mr-2 bg-blue-400 hover:bg-blue-500 text-white" 
                 onClick={()=> {
 
