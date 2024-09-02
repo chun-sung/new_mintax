c03c217a93fdc9a43845723f21162ea691a35b6d
SP_k <springstar74@gmail.com>
2024-09-02 14:32:37 +0900

    20240902_1417

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,8 @@ const advent_Pro = Advent_Pro({
 
 export const metadata = {
   title: "n-MinTax",
-  description: "Audit Company",  
+  description: "Audit Company",
+  themeColor: `${ mode?.value == undefined || mode?.value == 'lightMode' ? '#ffffff' : '#000000' }`
 };
 
 export default function RootLayout({ children }) {
@@ -34,7 +35,7 @@ export default function RootLayout({ children }) {
     <html lang="ko">            
       <link rel="icon" href="/favicon.ico" />
       <link rel="manifest" href="/manifest.json" />
-      <meta name="theme-color" content={`${ mode?.value == undefined || mode?.value == 'lightMode' ? '#ffffff' : '#000000' }`} />
+      {/* <meta name="theme-color" content={`${ mode?.value == undefined || mode?.value == 'lightMode' ? '#ffffff' : '#000000' }`} /> */}
 
       <body className={`relative ${inter.className} ${ mode?.value == undefined || mode?.value == 'lightMode' ? 'lightMode' : 'darkMode' }`}>        
           
