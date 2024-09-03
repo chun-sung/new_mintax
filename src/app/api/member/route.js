cc2e4c2a880f8cd097a3c720a045490bb8a1985e
unknown <you@example.com>
2024-09-03 10:15:58 +0900

    20240903_0946_first-Database2

--- src/app/api/member/route.js
+++ src/app/api/member/route.js
@@ -1,6 +1,6 @@
 // const {pool} = require('../../DB/db');
 import { pool } from '@vercel/postgres'
-// import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt'
 
 
 // 회원 정보 API 제공 (GET)
@@ -8,7 +8,7 @@ export async function GET(req) {
     const user = await pool`SELECT * FROM users`;
     let { rows }  =  data 
     console.log(rows)
-    // return new Response(JSON.stringify(user))
+    return new Response(JSON.stringify(user))
 }  
 
 // 회원 가입 (POST)
@@ -16,7 +16,7 @@ export async function POST(req) {
     let body = await req.json()
 
     try {
-        const results = await pool.query(`SELECT * FROM members WHERE user_id='${body.user_id}' `);    
+        const results = await pool`SELECT * FROM members WHERE user_id='${body.user_id}' `;    
         
         if(results.length == 1) {                                      // ID 중복 체크
             
@@ -25,11 +25,11 @@ export async function POST(req) {
     
             let hash = await bcrypt.hash(body.password, 10)
 
-            await pool.query(`
+            await pool`
                 INSERT INTO members(user_id, nickName ,password)
                 Values
                 ('${body.user_id}', '${body.nickName}', '${hash}') 
-            `);
+            `;
             return new Response(JSON.stringify({msg: 'success'}))
         }
     } catch (err) {
