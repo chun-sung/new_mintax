e8eef34c36d824fa3c09c3e0a1176b1ebb669469
SP_k <springstar74@gmail.com>
2024-09-09 15:42:32 +0900

    20240909_댓글 모양 및 컬러 수정4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -48,9 +48,14 @@ export default function Navbar({mode, setMode}) {
                     className="m-auto"            
                   />
             }
-            </Link>            
-            <span className="absolute lg:hidden bottom-[-4px] left-[130px] lg:left-[430px] text-blue-300 
-                border-[1px] border-blue-00 px-2 rounded-md text-sm">{user.user_id}</span>
+            </Link>
+            { user.user_id !== null ?
+              <span className="absolute lg:hidden bottom-[-4px] left-[140px] lg:left-[430px] text-blue-300 
+                  border-[1px] px-2 rounded-md text-sm">{user.user_id}</span>
+                :
+              <span className="absolute lg:hidden bottom-[-4px] left-[140px] lg:left-[430px] text-gray-300 
+                  px-2 rounded-md text-sm">Guest</span>
+            }            
         </div>
     </div> 
     <nav className={user.menu !== true  ? `hidden lg:block  lg:bg-slate-900 lg:text-white start lg:opacity-100` : 
