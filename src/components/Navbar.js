72ce21a0305aee90833d42f779e9c92d8cee4a80
unknown <you@example.com>
2024-09-25 16:35:33 +0900

    20240925

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -157,7 +157,7 @@ export default function Navbar({mode}) {
                 </button>
               </div>
 
-          : <div className="lg:inline-block mb-[15px] lg:mb-[0px]">
+          : <div className="lg:inline-block mb-[15px] lg:mb-[1.5px] lg:ml-[17px]">
               <p className="ml-[-45px] text-gray-400 lg:hidden">Guest</p>
               <button onClick={()=> { dispatch(SET_LOGIN_WINDOW(true));dispatch(SET_MENU_BTN(false));dispatch(SET_MEMBER_PANEL(false));}}
                 className="mt-0 lg:mt-0 min-w-[70px] ml-[-49px] lg:ml-[-5px] mr-[0px] lg:hover:bg-slate-700 text-blue-400 lg:text-blue-400 w-[65px] h-[27px] text-[14px] text-center rounded-l-xl
